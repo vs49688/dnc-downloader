7fafb08121f6fc980ba8e163e1748d2777b0be42
Zane van Iperen <vaniperen.zane@yahoo.com.au>
2016-07-23 23:20:32 +1000

    + Added error checking if cURL fails   - This happens if some of the file names are the same (which some are) * Prepended the email id to the output file

--- dncdownload.sh
+++ dncdownload.sh
@@ -3,13 +3,30 @@
 # curl -OJL https://wikileaks.org/dnc-emails//get/<id-here>
 # IDs range from [1, 22456]
 
-rm -rf eml output
-mkdir eml
-mkdir output
-cd eml
+OUTPUT_FILE="dncdownload.log"
+rm -rf eml output $OUTPUT_FILE
 
-for i in {1..22456};
-do
-	echo "Downloading $i..."
-	curl -OJL "https://wikileaks.org/dnc-emails//get/$i" > "../output/$i.stdout" 2> "../output/$i.stderr";
+exec {FD}>$OUTPUT_FILE
+
+mkdir eml output
+cd eml
+mkdir tmp
+cd tmp
+
+for i in {1..22456}; do
+	echo "Downloading $i..." | tee -a /dev/fd/$FD
+	curl -OJL "https://wikileaks.org/dnc-emails//get/$i" > "../../output/$i.stdout" 2> "../../output/$i.stderr"
+	CURLRET=$?
+
+	if [ $CURLRET -ne 0 ]; then
+		rm -rf *
+		echo " * Failed to download: cURL returned $CURLRET. See output/$i.stderr for more information." | tee -a /dev/fd/$FD
+		break
+	fi
+	EMLFILE=`ls -1 | head -1`
+	OUTFILE=`printf "%05d" $i`_$EMLFILE
+	mv "$EMLFILE" "../$OUTFILE"
 done
+
+cd ..
+rm -rf tmp
